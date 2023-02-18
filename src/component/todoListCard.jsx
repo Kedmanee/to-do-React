dc807316f1363bfebc83a3cff6376337a58eb215
kedmanee <63070012@kmitl.ac.th>
2023-02-18 20:13:03 +0700

    Add tag button

--- src/component/todoListCard.jsx
+++ src/component/todoListCard.jsx
@@ -10,11 +10,14 @@ import '../custom-styles.scss';
 import Modal from 'react-bootstrap/Modal';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import Tag from './tag';
 
 function Todo({ list }) {
     const [show, setShow] = useState(false);
     const [priority, setPriority] = useState('later');
+    const [tag, setTag] = useState('Home');
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const [todoname, setTodoname] = useState('');
@@ -23,7 +26,7 @@ function Todo({ list }) {
     }
     const saveNewList = () => {
         console.log(todoname, priority)
-        list[priority].push({name:todoname,tag:"test"})
+        list[priority].push({ name: todoname, tag: tag })
         setTodoname('')
         setShow(false)
     }
@@ -67,7 +70,7 @@ function Todo({ list }) {
                     </ListGroup>
                 </Card.Body>
             </Card>
-            
+
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title className='todo-title'>Add new list</Modal.Title>
@@ -79,7 +82,18 @@ function Todo({ list }) {
                         <Dropdown.Item onClick={() => setPriority('now')}>Now</Dropdown.Item>
                         <Dropdown.Item onClick={() => setPriority('later')}>Later</Dropdown.Item>
                     </DropdownButton>
-
+                    <p style={{ marginTop: '1em' }}>Select Tag</p>
+                    <ToggleButtonGroup type="radio" name="options" defaultValue={1} variant="custom-primary">
+                        <ToggleButton id="tbg-radio-1" value={1} variant="tag-color" onClick={() => setTag('Home')}>
+                           Home
+                        </ToggleButton>
+                        <ToggleButton id="tbg-radio-2" value={2} variant="tag-color" onClick={() => setTag('Uni')}>
+                            Uni
+                        </ToggleButton>
+                        <ToggleButton id="tbg-radio-3" value={3} variant="tag-color" onClick={() => setTag('Other')}>
+                            Other
+                        </ToggleButton>
+                    </ToggleButtonGroup>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
